5e2af6af07aee4063eeed5bf2e5bab61e7700d3d
Nazarena Aranda <nazarenaaranda06@gmail.com>
2025-01-20 02:09:46 +0000

    edges

---
+++ python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+letter3 = word[:3]
+print(f"First 3 letters: {letter3}")
+letter2 = word[-2:]
+print(f"Last 2 letters: {letter2}")
+middle = word[1:-1]
+print(f"Middle word: {middle}")
