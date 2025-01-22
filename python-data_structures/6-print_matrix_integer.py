afb2f6afeefcfdde33eeb4259d7bb98f8eeae0e5
Nazarena Aranda <nazarenaaranda06@gmail.com>
2025-01-22 23:42:08 +0000

    matrix with format

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
+    if matrix == [[]]:
+        return
     for row in matrix:
         for element in range(len(row)):
             if element < len(row) - 1:
