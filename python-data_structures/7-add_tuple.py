7100d7e9ca18f3e900b3a90aa641aef7779dd695
Nazarena Aranda <nazarenaaranda15@gmail.com>
2025-01-23 22:29:36 +0000

    add tuple

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -6,6 +6,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
     for i in range(2):
         if len(tuple_a) > i:
             res[i] = tuple_a[i]
-            if len(tuple_b) > i:
-                res[i] += tuple_b[i]
+        if len(tuple_b) > i:
+            res[i] += tuple_b[i]
     return tuple(res)
