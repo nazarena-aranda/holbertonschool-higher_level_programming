921032828eba818929c68afbaa2834e89295d3bf
Nazarena Aranda <nazarenaaranda06@gmail.com>
2025-01-24 20:30:50 +0000

    hight value

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,4 +3,11 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    return max(a_dictionary.keys())
+
+    best = 0
+    key = None
+    for k, v in a_dictionary.items():
+        if v > best:
+            best = v
+            key = k
+    return key
