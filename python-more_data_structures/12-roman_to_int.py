f3af6f1573f08caffecfa05ba4571648f6c46c14
Nazarena Aranda <nazarenaaranda06@gmail.com>
2025-01-24 21:47:07 +0000

    roman to int

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,12 +2,13 @@
 
 def roman_to_int(roman_string):
 
+    if not isinstance(roman_string, str):
+        return 0
+
     ref = {'I': 1, 'V': 5, 'X': 10, 'C': 100, 'D': 500, 'L': 50, 'M': 1000}
     num = 0
     prev = 0
-    
-    if not roman_string:
-        return 0
+
     for rn in reversed(roman_string):
         n = ref[rn]
         if prev > n:
