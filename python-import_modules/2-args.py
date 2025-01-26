4227eb7db1564c39d945a6a8465ecab5dc0003b7
Nazarena Aranda <nazarenaaranda15@gmail..com>
2025-01-26 20:48:15 +0000

    import sys

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 import sys
 
-args_len = len(sys.argv) - 1
+if __name__ = "__main__":
+    args_len = len(sys.argv) - 1
 
-if args_len <= 1:
-    print("{} argument:".format(args_len))
-else:
-    print("{} arguments:".format(args_len))
+    if args_len <= 1:
+        print("{} argument:".format(args_len))
+    else:
+        print("{} arguments:".format(args_len))
 
-idx = 1
-for arg in sys.argv[1:]:
-    print("{}: {}". format(idx, arg))
-    idx += 1
+    idx = 1
+    for arg in sys.argv[1:]:
+        print("{}: {}". format(idx, arg))
+        idx += 1
