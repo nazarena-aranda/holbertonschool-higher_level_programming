5f7d1f998ec793bda1491f14f20dbb47c4ea9b86
Nazarena Aranda <nazarenaaranda15@gmail..com>
2025-01-26 18:37:13 +0000

    print list using trt and except

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
     count = 0
 
@@ -7,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[i], end="")
             count += 1
-    except:
+    except IndexError:
         pass
     print()
     return count
