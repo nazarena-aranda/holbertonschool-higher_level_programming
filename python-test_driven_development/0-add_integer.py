426142918c578ee5d446420c1b33ad6676da54f2
Nazarena Aranda <nazarenaaranda15@gmail..com>
2025-01-27 01:09:01 +0000

    my first case of tests

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """
-In this package you will be able to see how 2 numbers add up. Valid number types are float or integer.
-Any other data type will show an exception. Floats are converted to integers by taking the first digit,
-so you can do the addition only with integers.
+In this package you will be able to see how 2 numbers add up. Valid number
+types are float or integer. Any other data type will show an exception.
+Floats are converted to integers by taking the first digit, so you can do
+the addition only with integers.
 """
 def add_integer(a, b=98):
     """
-    The function add_integer is a function which allows 2 parameters, “a” will be a number to be added with “b”.
+    The function add_integer is a function which allows 2 parameters, “a”
+    will be a number to be added with “b”.
     """
 
     if not isinstance(a, (int, float)):
