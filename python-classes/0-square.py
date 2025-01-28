cff8bce521f0fc27c77f8a8f6b881ba04b10e162
Nazarena Aranda <nazarenaaranda06@gmail.com>
2025-01-28 22:58:19 +0000

    class empty

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+"""
+In this module we are going to create an empty class,
+to create the class we use class and then to indicate
+that it has nothing we pass it the pass
+"""
+
 
 class Square:
+    """
+    class empty
+    """
     pass
