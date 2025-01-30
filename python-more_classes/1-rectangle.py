68cc254d6bee8d07d6d473bdc1b8e4208888fa5f
Nazarena Aranda <nazarenaaranda06@gmail.com>
2025-01-30 14:47:39 +0000

    class with instance

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -35,5 +35,5 @@ class Rectangle:
             raise TypeError("height must be an integer") 
         if value < 0:
             raise ValueError("height must be >= 0")
-        
+
         self.__height = value
