1d198db3311afb4bb8a79482eb4891a79fcf85ef
Nazarena Aranda <nazarenaaranda06@gmail.com>
2025-02-05 17:08:22 +0000

    list of available attributes and methods

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Create a function
+"""
+
+
+def lookup(obj):
+    """
+    function that returns the list of available
+    attributes and methods of an object using
+    dir
+    """
+    return dir(obj)
