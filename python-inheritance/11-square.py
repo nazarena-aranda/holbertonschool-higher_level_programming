d92dfa1cea2d28907eb5f7b846f154fd672cc8fb
Nazarena Aranda <nazarenaaranda06@gmail.com>
2025-02-07 20:54:33 +0000

    print str

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -4,36 +4,10 @@ create class
 """
 
 
-class BaseGeometry:
-    """
-    class BaseGeometry
-    """
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if not isinstance(value, int) or isinstance(value, bool):
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
-class Rectangle(BaseGeometry):
-    """
-    class that inherits from BaseGeometry
-    """
-    def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        return self.__height * self.__width
-
-    def __str__(self):
-        return f"[Rectangle] {self.__width}/{self.__height}"
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -4,18 +4,7 @@ create class
 """
 
 
-class BaseGeometry:
-    """
-    class BaseGeometry
-    """
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if not isinstance(value, int) or isinstance(value, bool):
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
