532a65f438155fd63257043ae32bcb26ab37a343
Nazarena Aranda <nazarenaaranda06@gmail.com>
2025-02-07 02:25:16 +0000

    my documentation with my_list

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """
+create a class MyList
 """
 
 
 class MyList(list):
     """
+    create a function that order a lists
     """
     def print_sorted(self):
         print(sorted(self))
