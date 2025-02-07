580b701fb03a0ca1bf2d0e6b9aa2ac13981b0c50
Nazarena Aranda <nazarenaaranda06@gmail.com>
2025-02-07 03:04:17 +0000

    raise exception

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+create class BaseGeometry
+"""
+
+
+class BaseGeometry():
+    """
+    class with raises
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
