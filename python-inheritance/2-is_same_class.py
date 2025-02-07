3e6aac5091fd9275f3db16237caa40c6a44cd2de
Nazarena Aranda <nazarenaaranda06@gmail.com>
2025-02-07 02:43:37 +0000

    my documentation with my_list

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+create a function is_same_class
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Returns True if obj is exactly an instance
+    of a_class, otherwise False."""
+    return type(obj) == a_class
