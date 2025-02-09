5f9ef2220e66f73908cb523c533440cabd9087a6
Nazarena Aranda <nazarenaaranda06@gmail.com>
2025-02-09 15:07:58 +0000

    task 0

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+import package ABC
+"""
+
+
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    @abstractmethod
+    def sound(self):
+        pass
+
+
+class Dog(Animal):
+    def sound(self):
+        return "Bark"
+
+
+class Cat(Animal):
+    def sound(self):
+        return "Meow"
