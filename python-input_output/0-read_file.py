624a436d6557fe36ec5d0911e3169883aa968d7e
Nazarena Aranda <nazarenaaranda06@gmail.com>
2025-02-14 20:44:19 +0000

    function that reads a text file and print the content

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -9,4 +9,4 @@ def read_file(filename=""):
     function that reads a text file
     """
     with open(filename, 'r', encoding='utf-8') as file:
-        print(file.read())
+        print(file.read(), end="")
