eb65472ad6558d68babc143339a652a291f8ffbe
Nazarena Aranda <nazarenaaranda06@gmail.com>
2025-02-14 22:59:14 +0000

    function that appends a string at the end of a text file

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import json
+"""
+module with function
+"""
+
+
+def to_json_string(my_obj):
+    """
+    function that returns the JSON representation of an object 
+    """
+    return json.dumps(my_obj)
