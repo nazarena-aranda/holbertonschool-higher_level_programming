3e3a57256642e2b3218c29287b2ef32d2bbe83a6
Nazarena Aranda <nazarenaaranda06@gmail.com>
2025-02-15 23:04:40 +0000

    many files

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -10,4 +10,4 @@ def load_from_json_file(filename):
     Write a function that creates an Object from a “JSON file”:
     """
     with open(filename, 'r', encoding='utf-8') as file:
-          return json.load(file)
+        return json.load(file)
