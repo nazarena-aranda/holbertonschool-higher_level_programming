84ae5fda549b2b0d3243ae5506b766bfa8c9a66d
Nazarena Aranda <nazarenaaranda06@gmail.com>
2025-02-17 02:59:15 +0000

    test

---
+++ python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+module with class
+"""
+class Student:
+    """
+    defines a student by first_name, last_name and age
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    
+    def to_json(self, attrs=None):
+        """
+        retrieves a dictionary representation of a Student instance; if attrs is a list of strings, only attribute names contained in this list must be retrieved
+        """
+        if attrs is None:
+            return self.__dict__
+        return {key: getattr(self, key) for key in attrs if key in self.__dict__}
---
+++ python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+module with class
+"""
+class Student:
+    """
+    defines a student by first_name, last_name and age
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    
+    def to_json(self, attrs=None):
+        """
+        retrieves a dictionary representation of a Student instance; if attrs is a list of strings, only attribute names contained in this list must be retrieved
+        """
+        if attrs is None:
+            return self.__dict__
+        return {key: getattr(self, key) for key in attrs if key in self.__dict__}
+    
+    def reload_from_json(self, json):
+        """
+        replaces all attributes of the Student instance with values from json dictionary
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
