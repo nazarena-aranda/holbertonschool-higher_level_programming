573307ab72fa25691e67d0d4ae7844e797f8aa9f
Nazarena Aranda <nazarenaaranda06@gmail.com>
2025-02-26 02:10:56 +0000

    task 02

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import requests
+import csv
+"""
+import module
+"""
+
+
+def fetch_and_print_posts():
+    """
+    function
+    """
+    response = requests.get("https://jsonplaceholder.typicode.com/posts")
+    status = response.status_code
+    posts = response.json()
+
+    print(f"Status Code: {status}")
+
+    for post in posts:
+        print(post["title"])
+
+
+def fetch_and_save_posts():
+    """
+    function 2
+    """
+    response = requests.get("https://jsonplaceholder.typicode.com/posts")
+    status = response.status_code
+
+    if status == 200:
+        posts = response.json()
+        with open("posts.csv", 'w') as file:
+            fieldnames = ["userId", "id", "title", "body"]
+            writer = csv.DictWriter(file, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerows(posts)
