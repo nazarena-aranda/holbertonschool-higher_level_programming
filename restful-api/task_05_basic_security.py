8400a60daa5b1c8b22fb5e8563c73b27fc7024c0
Nazarena Aranda <nazarenaaranda06@gmail.com>
2025-03-03 01:58:53 +0000

    task 05

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -23,25 +23,25 @@ users = {
     "admin1": {"username": "admin1", "password": generate_password_hash("password"), "role": "admin"}
 }
 
-  @jwt.unauthorized_loader
-  def handle_unauthorized_error(err):
-      return jsonify({"error": "Missing or invalid token"}), 401
+@jwt.unauthorized_loader
+def handle_unauthorized_error(err):
+    return jsonify({"error": "Missing or invalid token"}), 401
 
-  @jwt.invalid_token_loader
-  def handle_invalid_token_error(err):
-      return jsonify({"error": "Invalid token"}), 401
+@jwt.invalid_token_loader
+def handle_invalid_token_error(err):
+    return jsonify({"error": "Invalid token"}), 401
 
-  @jwt.expired_token_loader
-  def handle_expired_token_error(err):
-      return jsonify({"error": "Token has expired"}), 401
+@jwt.expired_token_loader
+def handle_expired_token_error(err):
+    return jsonify({"error": "Token has expired"}), 401
 
-  @jwt.revoked_token_loader
-  def handle_revoked_token_error(err):
-      return jsonify({"error": "Token has been revoked"}), 401
+@jwt.revoked_token_loader
+def handle_revoked_token_error(err):
+    return jsonify({"error": "Token has been revoked"}), 401
 
-  @jwt.needs_fresh_token_loader
-  def handle_needs_fresh_token_error(err):
-      return jsonify({"error": "Fresh token required"}), 401
+@jwt.needs_fresh_token_loader
+def handle_needs_fresh_token_error(err):
+    return jsonify({"error": "Fresh token required"}), 401
 
 # Here is a custom decorator that verifies the JWT is present in the request,
 # as well as insuring that the JWT has a claim indicating that this user is
