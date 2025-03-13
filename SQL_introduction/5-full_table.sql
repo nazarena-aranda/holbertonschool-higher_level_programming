c24fa91d2fb206c2eb40f8e900e3b1732fcff297
Nazarena Aranda <nazarena06@gmail.com>
2025-03-13 18:53:36 +0000

    sql introduction, tasks and README

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,6 +1,2 @@
-/*
-script that prints the following description of the table first_table from the database hbtn_0c_0 in your MySQL server.
-The database name will be passed as an argument of the mysql command
-You are not allowed to use the DESCRIBE or EXPLAIN statements
-*/
+-- script that prints the following description of the table first_table from the database hbtn_0c_0 in your MySQL server.
 SHOW CREATE TABLE first_table;
