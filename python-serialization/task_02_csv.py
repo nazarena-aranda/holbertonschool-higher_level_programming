9fde93b5828d388bdc44ae44534f06e42f3535b1
nazarena-aranda <nazarenaaranda06@gmail.com>
2025-04-08 18:46:44 -0300

    serialization

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -7,7 +7,7 @@ def convert_csv_to_json(csv_file):
             reader = csv.DictReader(f)
             data = list(reader)
             
-        with open(data.json, 'w') as f:
+        with open('data.json', 'w') as f:
             json.dump(data, f)
 
         return True
